89361ecc65b69d2e31a229d4837e8a1e60c7e484
brunofurmon <brunofurmon@poli.ufrj.br>
2016-10-07 12:10:08 -0300

    Enabling CodeFirst Migrations to alter Entities

--- ApiTest/Models/AbstractModel.cs
+++ ApiTest/Models/AbstractModel.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
 
 namespace ApiTest.Models
 {
     public class AbstractModel
     {
+        [Key]
         public long Id { get; set; }
-        public DateTime CreationDate { get; set; }
+
+        [DefaultValue("getutcdate()")]
+        public SqlDateTime CreationDate { get; set; }
     }
 }
