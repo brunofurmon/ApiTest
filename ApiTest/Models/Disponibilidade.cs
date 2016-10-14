40a4a893bafbd551e71cbc27fa8ab83b96029ba8
brunofurmon <brunofurmon@poli.ufrj.br>
2016-10-14 14:53:44 -0300

    Adds Disponibilidade Model

---
+++ ApiTest/Models/Disponibilidade.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ApiTest.Models
+{
+    public class Disponibilidade: AbstractModel
+    {
+        public decimal Preco { get; set; }
+        public decimal PrecoDe { get; set; }
+        public bool Disponivel { get; set; }
+        public string CodigoMarketplace { get; set; }
+
+            public int skuId { get; set; }
+        [ForeignKey("SkuId")]
+        public virtual Sku Sku { get; set; }
+    }
+}
